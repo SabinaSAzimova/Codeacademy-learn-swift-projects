dc37c979dbe8d8cfc6f5e63651f6385315606914
SabinaSAzimova <102950368+SabinaSAzimova@users.noreply.github.com>
2022-04-04 12:18:27 +0400

    Block Letters

---
+++ Initials.swift
@@ -0,0 +1,8 @@
+// Fun fact: I don't eat pizza.
+print(" S S S       A    ")
+print("S     S    A   A  ")
+print("S        A       A")
+print(" S S S   A A A A A")
+print("      S  A       A")
+print("S     S  A       A")
+print(" S S S   A       A")
