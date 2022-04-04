11dbe9ac9b671dce438c172531db178f37a571f3
SabinaSAzimova <102950368+SabinaSAzimova@users.noreply.github.com>
2022-04-04 13:17:54 +0400

    Comparison Operators

---
+++ Conditionals&Logic/SwiftJam.swift
@@ -0,0 +1,11 @@
+let tuneSquadPoints = 78
+let monstarsPoints = 77
+
+// Write your code below 🏀
+
+if tuneSquadPoints < monstarsPoints {
+  print("The winner is Monstars!")
+}
+else {
+   print("The winner is TuneSquad!")
+}
