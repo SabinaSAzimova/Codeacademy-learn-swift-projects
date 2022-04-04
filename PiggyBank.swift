34353bf57d23b064485d15291f4f1385ff58f4c1
SabinaSAzimova <102950368+SabinaSAzimova@users.noreply.github.com>
2022-04-04 12:31:17 +0400

    Piggy Bank

---
+++ PiggyBank.swift
@@ -0,0 +1,12 @@
+var pesos : Double = 10.50
+var reais : Double = 17.90
+var soles : Double = 5.70
+var total : Double
+
+// 1 peso = 0.050 usd
+// 1 real = 0.21 usd
+// 1 sole = 0.27 usd
+
+total = 0.050 * pesos + 0.21 * reais + 0.27 * soles
+
+print("US Dollars = $\(total)")
