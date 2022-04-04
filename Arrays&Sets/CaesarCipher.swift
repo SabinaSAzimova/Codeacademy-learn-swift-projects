52e04c13b04f423233273d66e8c4ef97f85c3116
Sabina Azimova <102950368+SabinaSAzimova@users.noreply.github.com>
2022-04-04 17:25:01 +0400

    Caesar's Cipher

---
+++ Arrays&Sets/CaesarCipher.swift
@@ -0,0 +1,16 @@
+var alphabet : [Character] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+var secretMessage = "codeacademy"
+print(secretMessage)
+var message = Array(secretMessage)
+//print(message)
+
+for i in stride(from:0, to:message.count, by:1) {
+  for j in stride(from:0, to:alphabet.count, by:1) {
+    if message[i]==alphabet[j] {
+      message[i]=alphabet[(j+3)%26]
+      break
+    }
+  }
+}
+print("In Caesar Chiper \(secretMessage)  is \(String(message)).")
