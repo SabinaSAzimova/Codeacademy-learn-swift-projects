276bcbed273c949beed6758b773b9c876d954698
SabinaSAzimova <102950368+SabinaSAzimova@users.noreply.github.com>
2022-04-04 15:22:50 +0400

    .append() Method

---
+++ Arrays&Sets/NewYearNewMe.swift
@@ -0,0 +1,9 @@
+var resolutions = ["play more music 🎸",
+                   "read more books 📚",
+                   "drink more water 💧"]
+
+// Write your code below 💪
+
+resolutions.append("learn swift language")
+resolutions += ["buy a new car", "buy a new house"]
+print(resolutions)
