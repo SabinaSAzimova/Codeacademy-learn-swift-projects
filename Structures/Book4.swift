3b4a18c97b698f920813c01dc8d49ccc4add9d44
Sabina Azimova <102950368+SabinaSAzimova@users.noreply.github.com>
2022-04-05 12:20:00 +0400

    Accessing and Editing Properties

---
+++ Structures/Book4.swift
@@ -0,0 +1,14 @@
+struct Book {
+  var pages = 0
+  var title = ""
+}
+
+var myFavBook = Book()
+
+// Write your code below: 📚
+
+print(myFavBook.pages)
+myFavBook.pages = 640
+print(myFavBook.pages)
+myFavBook.title = "To Kill a Mockingbird"
+print(myFavBook.title)
