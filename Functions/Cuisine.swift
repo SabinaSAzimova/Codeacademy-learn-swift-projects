b40110752d0ff8581b7f14fe8b9835431f032a96
Sabina Azimova <102950368+SabinaSAzimova@users.noreply.github.com>
2022-04-05 10:39:08 +0400

    Returning Multiple Values

---
+++ Functions/Cuisine.swift
@@ -0,0 +1,8 @@
+// Write your code below 😋
+
+func favoriteCuisine() -> (name: String, dish: String ) {
+  return("Azerbaijani","Kebab")
+}
+
+let cuisine = favoriteCuisine()
+print("My favorite \(cuisine.name) dish is \(cuisine.dish)!")
