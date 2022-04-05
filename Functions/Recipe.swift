739f1d422220cde9e6399c9eac5e75ee1dec8e62
Sabina Azimova <102950368+SabinaSAzimova@users.noreply.github.com>
2022-04-05 06:04:22 +0400

    Calling a Function

---
+++ Functions/Recipe.swift
@@ -0,0 +1,10 @@
+// Write your code below 🍲
+
+func mySpecialRecipe() {
+  print("Take some colorful bell peppers and cut them in stripes")
+  print("Boil chicken breasts and cut into julienne")
+  print("Add salt and pepper to mayonaisse and mix it well with the peppers and chicken.")
+  print("Bon appetite!")
+}
+
+mySpecialRecipe()
